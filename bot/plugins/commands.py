590bb062bd627aa30ec87f5eb5170f544c07993a
danishmdk222 <85662224+danishmdk222@users.noreply.github.com>
2021-06-14 08:59:17 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -30,7 +30,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = f"{ file_name} \n @Healerv2",
+                caption = f"{file_name} \n @Healerv2",
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
@@ -38,7 +38,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    ' 🧿Movie Group 🧿 ', url="https://t.me/pskmoviescompanydiscussion"
+                                    ' ⚡🧿Movie Group🧿⚡ ', url="https://t.me/pskmoviescompanydiscussion"
                                 )
                         ]
                     ]
